5268bad9314bd0d482ceaa6f65bdb3e76080d1e7
louielolo <lu.yanting@qq.com>
2021-06-30 18:13:43 +0800

    update

--- basic/main.cpp
+++ basic/main.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 int main()
 {
-    std::cout <<"Enter two numbers"<< std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
+    //std为namespace避免名字冲突，::为作用域运算符，指出命名空间std中的cout
+    std::cout <<"Enter two numbers"<< std::endl;//endl可将与设备关联缓冲区刷到设备，不是停留在内存等着输入流
+    int v1 = 0, v2 = 0;//初始化定义
+    std::cin >> v1 >> v2;//箭头表示流向，从输入流流到变量中
     std::cout <<"The sum of "<< v1 << " and "<<v2
               <<" is "<<v1+v2<< std::endl;
     return 0;
