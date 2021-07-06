f603b13c3b35195b52190efb468ec53c8f186888
louielolo <lu.yanting@qq.com>
2021-07-06 14:52:14 +0800

    update

---
+++ basic/arithmetric_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    unsigned a = 0;
+    int a1 = 0;
+    int a2 = 0;
+    unsigned b = 0;
+    unsigned b1 = 0;
+    unsigned b2 = 0;
+    unsigned char c = 0;
+    unsigned char c1 = 0;
+    unsigned char c2 = 0;
+    std::cin>>a>>b;
+    std::cout<<a-b<<std::endl;
+    std::cout<<b-a<<std::endl;
+
+}
--- basic/bookshop_cprime.cpp
+++ basic/bookshop_cprime.cpp
@@ -1,10 +1,37 @@
-#include "Sales_item.h"
-#include <iostream>
+#include<iostream>
+#include"Sales_item.h"
+using namespace std;
 
-int main() {
-    Sales_item book;
-    std::cin>>book;
-    std::cout<<book<<std::endl;
-    return 0;
+int main()
+{
+	Sales_item total;
 
+	cout << "Please input the bookNo units_sold price:" << endl;
+	if (cin >> total)
+	{
+		int cnt = 1;
+        Sales_item trans;
+		
+		while (cin >> trans)
+		{
+			if (trans.isbn() == total.isbn())
+			{
+				total += trans;
+				++cnt;
+			}
+			else
+			{
+				cout << total.isbn() << " occurs "
+					<< cnt << " times" << endl;
+				total = trans;
+				cnt = 1;
+			}
+		}
+		cout << total.isbn() << " occurs "
+			<< cnt << " times" << endl;
+	}
+	else
+		cerr << "enter error!";
+
+	return 0;
 }
